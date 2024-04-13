ee156c1e41480cfae47a14d46f05a55608916b52
abdullahalsehaim <abdullahsehaim85@gmail.com>
2024-04-13 17:46:02 +0000

    Created play game function

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+/**
+ * Adding DOM Constants
+ */
+const buttons = document.getElementsByClassName("btn");
+const score = document.getElementById("score");
+const userImage = document.getElementById("user-image");
+const computerImage = document.getElementById("computer-image");
+const outcomeSpan = document.getElementById("outcome");
+const choices = ["rock", "paper", "scissors", "spock", "lizard"];
+
+
+/**
+ * Adding a listener event for all game related buttons
+ */
+for (let button of buttons) {
+    button.addEventListener("click", function (){
+        let userChoice = this.getAttribute("data-choice");
+        playGame(userChoice);
+    })
+}
+
+/**
+ * The function that starts the game, function takes effect whenever user makes a choice of button
+ */
+
+function playGame(userChoice) {
+
+    userImage.src = `assets/images/${choices[userChoice]}.png`;
+    userImage.alt = choices[userChoice];
+
+    let computerChoice = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+    //determineWinner(userChoice, computerChoice);
+
+    //let outcome = '';
+
+    if (choices[userChoice] === choices[computerChoice]) {
+        outcome = "It's a tie!"
+    } else if (
+        (choices[userChoice] === "rock" && (choices[computerChoice] === "scissors" || choices[computerChoice] === "lizard")) ||
+        (choices[userChoice] === "paper" && (choices[computerChoice] === "rock" || choices[computerChoice] === "spock")) ||
+        (choices[userChoice] === "scissors" && (choices[computerChoice] === "paper" || choices[computerChoice] === "lizard")) ||
+        (choices[userChoice] === "spock" && (choices[computerChoice] === "scissors" || choices[computerChoice] === "rock")) ||
+        (choices[userChoice] === "lizard" && (choices[computerChoice] === "spock" || choices[computerChoice] === "paper"))) {
+        outcome = "You win!";
+    } else {
+        outcome = "You lose!";
+    }
+
+    console.log(outcome)
+    outcomeSpan.textContent = outcome;
+
+
+
+}
+
+/**
+ * This function will compare the users and computers choice to determine who has won
+ */
+
+
+function increaseScore () {
+
+}
